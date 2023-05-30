00e51769a36121d50c2393626e620183f61250ee
Ganivada Mouli <vishnumouli0@gmail.com>
2023-05-30 19:52:47 +0530

    keeper app starting files crested

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+import "/public/styles.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is the Title</h1>
+      <p>This is the Content</p>
+    </div>
+  );
+}
+
+export default Note;
