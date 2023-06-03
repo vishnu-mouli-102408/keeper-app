ad98e9ba0eba9680c11f1c4ecff291284fc889c1
Ganivada Mouli <vishnumouli0@gmail.com>
2023-06-03 15:12:27 +0530

    keeper app part-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,42 @@ import Header from "./Header";
 import Note from "./Note";
 // import "/public/styles.css";
 import "../../public/styles.css";
-import notes from "../notes";
+// import notes from "../notes";
+import CreateArea from "./CreateArea";
+import { useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      {notes.map((x) => (
-        <Note key={x.key} title={x.title} content={x.content} />
-      ))}
+      <CreateArea addNote={addNote} />
+      {notes.map((item, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={item.title}
+            content={item.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
